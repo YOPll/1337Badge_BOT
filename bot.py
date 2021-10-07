639ffb27075f9f570f4f3c1d7680da0c60fd9d0a
yopll <mrx.ga10@gmail.com>
2021-10-07 22:56:36 +0100

    need some fixes

---
+++ bot.py
@@ -0,0 +1,53 @@
+from pyppeteer import launch
+from time import sleep
+import discord
+import creds
+from discord.ext import commands
+from os import remove
+
+bot = commands.Bot(command_prefix='*', description='Your Description')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('Future is loading ...')
+    print('----------------------------------------------------------')
+    await bot.change_presence(status=discord.Status.online,activity=discord.Game(name='*help || YOPI'))
+
+@bot.command()
+async def pooler(ctx):
+    browser = await launch()
+    page = await browser.newPage()
+    user = ctx.message.content.split(" ")[1]
+    await page.setViewport({'width': 1, 'height': 1})
+    await page.goto('https://1337-readme.vercel.app/api/profile?cursus=c-piscine&dark=true&login={0}'.format(user))
+    sleep(1)
+    await page.screenshot({'path': 'example.png', 'fullPage': 'false'})
+    await browser.close()
+    file = discord.File("example.png")
+    remove("example.png")
+    await ctx.send(file=file, content="<@{0.author.id}>".format(ctx))
+
+@bot.command()
+async def student(ctx):
+    browser = await launch()
+    page = await browser.newPage()
+    user = ctx.message.content.split(" ")[1]
+    await page.setViewport({'width': 1, 'height': 1})
+    await page.goto('https://1337-readme.vercel.app/api/profile?cursus=42&dark=true&login={0}'.format(user))
+    sleep(1)
+    await page.screenshot({'path': 'example.png', 'fullPage': 'false'})
+    await browser.close()
+    file = discord.File("example.png")
+    remove("example.png")
+    await ctx.send(file=file, content="<@{0.author.id}>".format(ctx))
+
+@bot.group()
+async def owner(ctx):
+        yopi = discord.Embed(title = 'Owned by YOPI', description = f'Check out my **[twitter](https://twitter.com/YONINUX)** and **[github](https://github.com/YOPll)** ',color = discord.Colour.teal())
+        await ctx.send(embed = yopi)
+
+
+bot.run(creds.token)
