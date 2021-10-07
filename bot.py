9b3b36bb86a61d81e941501f1d1b720dce575464
yopll <mrx.ga10@gmail.com>
2021-10-08 00:58:14 +0100

    Done

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from os import remove
 
 bot = commands.Bot(command_prefix='*', description='Your Description')
 
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -15,6 +16,7 @@ async def on_ready():
     print('Future is loading ...')
     print('----------------------------------------------------------')
     await bot.change_presence(status=discord.Status.online,activity=discord.Game(name='*help || YOPI'))
+
 
 @bot.command()
 async def pooler(ctx):
@@ -32,22 +34,29 @@ async def pooler(ctx):
         remove("example.png")
         await ctx.send(file=file, content="<@{0.author.id}>".format(ctx))
     else:
-        yopi = discord.Embed(title = 'Syntax_error', description = f'Sorry <@{author}> Please enter login, Example : ***pooler login**',color = ctx.author.color)
+        yopi = discord.Embed(title = 'Syntax_error', description = f'Sorry <@{author}> Please enter pooler login, Example : ***pooler login**',color = ctx.author.color)
         await ctx.send(embed = yopi)
+
 
 @bot.command()
 async def student(ctx):
-    browser = await launch()
-    page = await browser.newPage()
-    user = ctx.message.content.split(" ")[1]
-    await page.setViewport({'width': 1, 'height': 1})
-    await page.goto('https://1337-readme.vercel.app/api/profile?cursus=42&dark=true&login={0}'.format(user))
-    sleep(1)
-    await page.screenshot({'path': 'example.png', 'fullPage': 'false'})
-    await browser.close()
-    file = discord.File("example.png")
-    remove("example.png")
-    await ctx.send(file=file, content="<@{0.author.id}>".format(ctx))
+    author = ctx.message.author.id
+    if len(ctx.message.content.split()) == 2:
+        browser = await launch()
+        page = await browser.newPage()
+        user = ctx.message.content.split(" ")[1]
+        await page.setViewport({'width': 1, 'height': 1})
+        await page.goto('https://1337-readme.vercel.app/api/profile?cursus=42&dark=true&login={0}'.format(user))
+        sleep(1)
+        await page.screenshot({'path': 'example.png', 'fullPage': 'false'})
+        await browser.close()
+        file = discord.File("example.png")
+        remove("example.png")
+        await ctx.send(file=file, content="<@{0.author.id}>".format(ctx))
+    else:
+        yopi = discord.Embed(title = 'Syntax_error', description = f'Sorry <@{author}> Please enter student login, Example : ***student login**',color = ctx.author.color)
+        await ctx.send(embed = yopi)
+
 
 @bot.group()
 async def owner(ctx):
