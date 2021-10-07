2dcfab50904d7194531cee6d4cd8d9b294c68eb7
yopll <mrx.ga10@gmail.com>
2021-10-08 00:55:33 +0100

    Done

--- bot.py
+++ bot.py
@@ -18,17 +18,22 @@ async def on_ready():
 
 @bot.command()
 async def pooler(ctx):
-    browser = await launch()
-    page = await browser.newPage()
-    user = ctx.message.content.split(" ")[1]
-    await page.setViewport({'width': 1, 'height': 1})
-    await page.goto('https://1337-readme.vercel.app/api/profile?cursus=c-piscine&dark=true&login={0}'.format(user))
-    sleep(1)
-    await page.screenshot({'path': 'example.png', 'fullPage': 'false'})
-    await browser.close()
-    file = discord.File("example.png")
-    remove("example.png")
-    await ctx.send(file=file, content="<@{0.author.id}>".format(ctx))
+    author = ctx.message.author.id
+    if len(ctx.message.content.split()) == 2:
+        browser = await launch()
+        page = await browser.newPage()
+        user = ctx.message.content.split(" ")[1]
+        await page.setViewport({'width': 1, 'height': 1})
+        await page.goto('https://1337-readme.vercel.app/api/profile?cursus=c-piscine&dark=true&login={0}'.format(user))
+        sleep(1)
+        await page.screenshot({'path': 'example.png', 'fullPage': 'false'})
+        await browser.close()
+        file = discord.File("example.png")
+        remove("example.png")
+        await ctx.send(file=file, content="<@{0.author.id}>".format(ctx))
+    else:
+        yopi = discord.Embed(title = 'Syntax_error', description = f'Sorry <@{author}> Please enter login, Example : ***pooler login**',color = ctx.author.color)
+        await ctx.send(embed = yopi)
 
 @bot.command()
 async def student(ctx):
